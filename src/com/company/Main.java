05f9268a97605162beccd3e47b1524ee416f877a
Navjot singh <bunnythakur1414@gmail.com>
2018-05-22 17:13:47 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+ private static Scanner scanner = new Scanner(System.in );
+
+    public static void main(String[] args) {
+        System.out.println(" enter 3 integer value ");
+       int var = scanner.nextInt();
+       int var1 =scanner.nextInt();
+       int var2 = scanner.nextInt();
+        System.out.println("myInt is: " + var );
+        System.out.println("myInt is: " + var1 );
+        System.out.println("myInt is: " + var2 );
+
+
+
+    }
+}
